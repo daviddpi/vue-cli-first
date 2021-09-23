40f58a26a32912655a60c3d7305d2193ea5b098d
daviddpi <piscopodavid97@gmail.com>
2021-09-23 17:25:39 +0200

    completato esercizio vue cli

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <img src="https://coreui.io/images/ogimages/vue_1200_600.jpg" alt="Jumbotron">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Jumbotron'
+}
+</script>
+
+<style>
+img{
+    width: 70%;
+    margin-top: 50px;
+}
+</style>
